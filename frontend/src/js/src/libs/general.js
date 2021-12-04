6c7c3fb321ad42ed439d3a75c51a8f3362f8434c
iznyn <mahtunis@gmail.com>
2021-12-04 15:44:30 +0700

    Fix general scroll script

--- frontend/src/js/src/libs/general.js
+++ frontend/src/js/src/libs/general.js
@@ -4,15 +4,27 @@
 'use strict';
 
 const General = () => {
-  $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+  // $(window).scroll(function() {    
+  //   var scroll = $(window).scrollTop();
 
-    if (scroll >= 50) {
-        $(".header").addClass("scroll");
+  //   if (scroll >= 50) {
+  //       $(".header").addClass("scroll");
+  //   } else {
+  //       $(".header").removeClass("scroll");
+  //   }
+  // });
+
+  window.addEventListener('scroll', () => {
+    const doc = document.documentElement;
+    const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    const headerEl = document.querySelector('.header');
+
+    if (top >= 50) {
+      headerEl.classList.add('scroll');
     } else {
-        $(".header").removeClass("scroll");
+      headerEl.classList.remove('scroll');
     }
-});
+  });
 };
 
 export default General;
