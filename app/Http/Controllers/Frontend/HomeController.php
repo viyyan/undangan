e90a1cb39518c9c16e75fed057617554955a0bf5
viyyan <yuliantoalfian@rocketmail.com>
2022-03-31 20:25:40 +0700

    initial commit

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Post;
-use App\Models\Banner;
 
 class HomeController extends Controller
 {
@@ -16,22 +14,15 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        $banners = Banner::where("status", 1)
-                    ->orderBy("order", "asc")->get();
-
-        $posts = Post::where("status", 1)
-            ->orderBy("featured", "desc")
-            ->orderBy("created_at", "desc")
-            ->take(3)
-            ->get();
 
         $data = array(
-            "cssFileName" => "home",
-            "jsFileName" => "home",
-            "cssBody" => "home",
-            "banners" => $banners,
-            "posts" => $posts
+            "figmaUrl" => "https://www.figma.com/proto/6xS0JoS93lS6Fkcxxi1JQj/Elektron"
+            // "cssFileName" => "home",
+            // "jsFileName" => "home",
+            // "cssBody" => "home",
+            // "banners" => $banners,
+            // "posts" => $posts
         );
-        return view('frontend.pages.home.main', $data);
+        return view('frontend.pages.figma.main', $data);
     }
 }
--- routes/frontend.php
+++ routes/frontend.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
-use App\Http\Controllers\Frontend\ContactController;
-use App\Http\Controllers\Frontend\ProductController;
-use App\Http\Controllers\Frontend\ParentingTipsController;
-use App\Http\Controllers\Frontend\WhereToBuyController;
 
 
 /*
@@ -21,34 +17,3 @@ use App\Http\Controllers\Frontend\WhereToBuyController;
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
 
-
-Route::prefix('product')->group(function () {
-    Route::get('/', [ProductController::class, 'index'])
-    ->name('product');
-    Route::get('/sirup', [ProductController::class, 'syrup'])
-        ->name('product.syrup');
-    Route::get('/drop', [ProductController::class, 'drop'])
-    ->name('product.drop');
-    Route::get('/expectorant', [ProductController::class, 'expectorant'])
-    ->name('product.expectorant');
-});
-
-
-Route::prefix('parenting-tips')->group(function () {
-    Route::get('/', [ParentingTipsController::class, 'index'])
-        ->name('parenting-tips');
-    Route::get('/{slug}', [ParentingTipsController::class, 'show'])
-        ->name('parenting-tips.details');
-});
-
-Route::get('/where-to-buy', [WhereToBuyController::class, 'index'])
-    ->name('where-to-buy');
-
-Route::get('contact-us', [ContactController::class, 'index'])
-    ->name('contact-us');
-
-Route::post('contact-us', [ContactController::class, 'submitInquiry'])
-    ->name('contact-us.submit');
-
-Route::view('privacy-policy', 'frontend.pages.privacy-policy.main', ["cssFileName" => "parenting"])
-    ->name('privacy-policy');
