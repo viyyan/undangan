e317788be54f76d6f7c1dd188cc6d09792772bae
viyyan <yuliantoalfian@rocketmail.com>
2022-04-04 14:03:52 +0700

    create base

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -7,16 +7,10 @@
     <div class="container">
         <div class="footer--logo">
             <a href="">
-                <img src="{{ frontImages('footer-logo.svg') }}">
             </a>
         </div>
         <div class="footer--center">
             <div class="footer--address">
-                <p>
-                    PT Transfarma Medica Indah Suite 802, 8th Fl., Wisma Pondok Indah 2<br> Jl.
-                    Sultan Iskandar Muda Blok V - TA Pondok Indah Jakarta 12310, Indonesia<br>
-                    Ph. +62-21-769-7323 | Fax. +62-21-769-7150
-                </p>
             </div>
             <div class="footer--social">
                 <ul>
@@ -31,12 +25,11 @@
         </div>
         <div class="footer--bottom">
             <div class="footer--copyright">
-                <span>Copyright &copy; 2022 Nipe Fever All rights reserved</span>
             </div>
             <div class="footer--nav">
                 <ul>
                     <li>
-                        <a href="{{ route('privacy-policy') }}">Privacy Policy</a>
+                        <a href="">Privacy Policy</a>
                     </li>
                 </ul>
             </div>
