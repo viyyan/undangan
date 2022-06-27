ddb2d0e1eedf0b28664bc8967aa23ea5d857129d
viyyan <yuliantoalfian@rocketmail.com>
2022-06-27 20:04:36 +0700

    add send message

--- app/Orchid/Layouts/Guest/GuestListLayout.php
+++ app/Orchid/Layouts/Guest/GuestListLayout.php
@@ -6,6 +6,9 @@ use App\Models\Guest;
 use Orchid\Screen\TD;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Layouts\Table;
+use Orchid\Screen\Actions\Button;
+use Carbon\Carbon;
+
 
 class GuestListLayout extends Table
 {
@@ -42,6 +45,46 @@ class GuestListLayout extends Table
                 ->render(function (Guest $guest) {
                     return $guest->created_at->format('h:i, d F Y');
                 }),
+
+            TD::make('phone', 'Send')
+                ->render(function (Guest $guest) {
+                    $msg = $this->composeMessage($guest);
+                    return "<div class=\"form-group mb-0\"><a href='{$msg}' class='btn btn-success' target='_blank'> Send </a></div>";
+                }),
         ];
+    }
+
+    /**
+     * @param Guest    $guest
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function composeMessage(Guest $guest)
+    {
+        $msg = "
+        Kepada Yth,
+        Bapak/Ibu/Saudara/i
+        $guest->name
+        بِسْــــــــمِ اٌللَّهِ اٌلرَّحْمَنِ اٌلرَّحِيْـــــــــمِ
+
+        Assalamualaikum Warahmatullahi Wabarakatuh
+
+        Untuk teman-teman sekalian, dengan sampainya pesan ini saya dan pasangan ingin menyampaikan kabar gembira kepada teman-teman sekalian. Dalam rangka akan dilangsungkannya acara Akad Nikah saya dan pasangan pada:
+
+        Hari/Tanggal: ".Carbon::parse($guest->event->event_date)->translatedFormat('l, d F Y')."
+        Tempat: Bogor
+        [link undangan]
+
+        Sebelumnya kami sampaikan permohonan maaf yang sebesar-besarnya karena satu dan lain hal kami belum bisa mengundang teman-teman ke acara pernikahan kami.
+
+        Sekali lagi, kami memohon doa restu dari teman-teman sekalian untuk acara Akad Nikah kami nanti.
+
+        Terima kasih.
+        Wassalamualaikum Warahmatullahi Wabarakatuh
+
+        Regards,
+        Tyas & Fian";
+        $msgEncode = urlencode($msg);
+        $url = "https://wa.me/$guest->phone?text=$msgEncode";
+        return $url;
     }
 }
--- app/Orchid/Screens/Guest/GuestListScreen.php
+++ app/Orchid/Screens/Guest/GuestListScreen.php
@@ -63,4 +63,6 @@ class GuestListScreen extends Screen
             GuestListLayout::class
         ];
     }
+
+
 }
